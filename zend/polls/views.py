cf815bdf3c57db163466a4539c62e9f4edd6ce6a
ballwql <ballwql@gmail.com>
2014-01-07 22:06:20 +0800

    update zend

--- zend/polls/views.py
+++ zend/polls/views.py
@@ -1,25 +1,60 @@
-from django.shortcuts import render
-from zend.settings import STATIC_URL
-from django.http import HttpResponse
-from polls.models import Poll
-from django.template import RequestContext,loader
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from polls.models import Poll,Choice
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name='polls/index.html'
+    context_object_name='latest_poll_list'
+    def get_queryset(self):
+        '''return the last five published polls.'''
+        return Poll.objects.order_by('-pub_date')[:5]
+class DetailView(generic.DetailView):
+    model=Poll
+    template_name='polls/detail.html'
+class ResultsView(generic.DetailView):
+    model=Poll
+    template_name='polls/results.html'
+def vote(request,poll_id):
+    p=get_object_or_404(Poll,pk=poll_id)
+    try:
+        selected_choice=p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        #redisplay the poll voting form
+        return render(request,'polls/detail.html',{'poll':p, 'error_message':"You didn't select a choice.",})
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        #return an httpresponseredirect after dealing successfully
+        return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
+    
+'''
 def index(request):
     latest_poll_list=Poll.objects.order_by('-pub_date')[:5]
-#    template = loader.get_template('polls/index.html')
-#    context=RequestContext(request,{'latest_poll_list':latest_poll_list,})
-#    return HttpResponse(template.render(context))
-    #shortcut
+    
+    #shortcut by render
     context={'latest_poll_list':latest_poll_list}
     return render(request,'polls/index.html',context)
     
 def detail(request,poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
-def results(request,poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
-def vote(request,poll_id):
-    return HttpResponse("youre voting on poll %s." %poll_id)
-#def loginpage(request):
-#    static_url=STATIC_URL
- #   return render_to_response('admin/login.html',locals())
+    
+    poll=get_object_or_404(Poll,pk=poll_id)
+    return render(request,'polls/detail.html',{'poll':poll})      
 
-# Create your views here.
+def results(request,poll_id):
+    poll=get_object_or_404(Poll,pk=poll_id)
+    return render(request,'polls/results.html',{'poll':poll})
+def vote(request,poll_id):
+    p=get_object_or_404(Poll,pk=poll_id)
+    try:
+        selected_choice=p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        #redisplay the poll voting form
+        return render(request,'polls/detail.html',{'poll':p, 'error_message':"You didn't select a choice.",})
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        #return an httpresponseredirect after dealing successfully
+        return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
+'''
